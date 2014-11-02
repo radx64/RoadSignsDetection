c3fcba5b9f2be75ddda369223649cd8a182bf9ff
radX64 <radek-team@wp.pl>
2014-11-02 09:11:23 +0100

    Few tweaks of hopfield network, little refactoring

--- scripts/hopfield.py
+++ scripts/hopfield.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
-
 import numpy as np
 import random
 
-
 trainSet = np.array(
 		[
-			[-1,-1,-1,-1],
-			[-1, 1, 1,-1]
+			[-1,-1, 1, 1,-1,-1],
+			[ 1, 1,-1,-1, 1, 1]
 		])
 
 print "Prepared trainSet shape: " + str(trainSet.shape)
@@ -16,50 +14,57 @@ class HopfieldNetwork:
 	"""A simple implementation of Hopfield Network"""
 
 	def __init__(self, neuronsCount, learningRate):
-		self.n = neuronsCount
+		self.neuronsCount = neuronsCount
 		self.learningRate = learningRate
 		self.weightsMatrix = np.array([])
 		self.outputMatrix = np.array([])
 
+	def getNeuronsMatrix(self):
+		return self.outputMatrix
+
+	def getWeightsMatrix(self):
+		return self.weightsMatrix
+
 	def initWeights(self, numberOfNeurons):
 		self.weightsMatrix = np.zeros((numberOfNeurons, numberOfNeurons))
 		print "Weights of net were initialized."
-		print self.weightsMatrix
 
-	def initNetwork(self, desiredNeuronsStateMatrix):
-		print self.outputMatrix
+	def initNeurons(self, desiredNeuronsStateMatrix):
 		self.outputMatrix = desiredNeuronsStateMatrix
-		print self.outputMatrix
-		print "Network was initilized with:"
-		print desiredNeuronsStateMatrix
+		print "Network was initilized."
 
-	def updateNetwork(self, updatesNumber):
+	def update(self, updatesNumber):
 		for u in range(updatesNumber):
-			neuron = random.randint(0,self.n-1)
-			print "Updating neuron: " + str(neuron)
+			neuron = random.randint(0,self.neuronsCount-1)
+			#print "Updating neuron: " + str(neuron)
 			activation = 0
-			for i in range(self.n):
+			for i in range(self.neuronsCount):
 				activation += self.outputMatrix[i] * self.weightsMatrix[neuron][i]
-				print "Activation value:" + str(activation)
+				#print "Activation value:" + str(activation)
 				self.outputMatrix[neuron] = 1 if activation > 0 else -1
-			print self.outputMatrix
+			#print self.outputMatrix
 
 	def train(self, trainingMatrix):
 		print "Training net"
-		for i in range(self.n):
-			for j in range(self.n):
+		for i in range(self.neuronsCount):
+			for j in range(self.neuronsCount):
 				if i != j:
 					self.weightsMatrix[i][j] += self.learningRate * trainingMatrix[i] * trainingMatrix[j]
 		print self.weightsMatrix
 
 def main():
 	net = HopfieldNetwork(4,0.3)
-	net.initWeights(4)
-	net.train([-1,-1,1,1])
-	net.train([1,1,-1,-1])
-	net.initNetwork([1,1,1,-1])
-	net.updateNetwork(10)
-	pass
+	net.initWeights(6)
+	net.train(trainSet[0])
+	net.train(trainSet[1])
+	net.initNeurons([1, 1, 1, 1,-1,-1])
+	net.update(10)
+	print "Results after 10 updates:"
+	print net.getNeuronsMatrix()
+	net.initNeurons([1, 1, 1,-1, 1, 1])
+	net.update(10)
+	print "Results after 10 updates:"
+	print net.getNeuronsMatrix()
 
 if __name__ == '__main__':
 	main()
