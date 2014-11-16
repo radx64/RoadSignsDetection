31284cbb058d461fd8ed4514d6ad34952d2f58f0
radX64 <radek-team@wp.pl>
2014-11-16 17:23:59 +0100

    Added simple comparision of remembering shape progress

--- scripts/main.py
+++ scripts/main.py
@@ -22,18 +22,30 @@ def main():
 	net.trainHebb(imageForHopfield)
 	net.initNeurons(np.ones(imageForHopfield.shape[0]))
 
-	net.update(1000)
-	print "Results after 1000 updates:"
-	remeberedImage = net.getNeuronsMatrix()
-	print remeberedImage
+	net.update(100)
+	remeberedImage100 = net.getNeuronsMatrix().copy()
+	remeberedImage100 = np.reshape(remeberedImage100, (image.shape[0],image.shape[0]))
+	net.update(200)
+	remeberedImage300 = net.getNeuronsMatrix().copy()
+	remeberedImage300 = np.reshape(remeberedImage300, (image.shape[0],image.shape[0]))
+	net.update(300)
+	remeberedImage600 = net.getNeuronsMatrix().copy()
+	remeberedImage600 = np.reshape(remeberedImage600, (image.shape[0],image.shape[0]))
+	net.update(400)
+	remeberedImage1000 = net.getNeuronsMatrix().copy()
+	remeberedImage1000 = np.reshape(remeberedImage1000, (image.shape[0],image.shape[0]))
 
-	remeberedImage = np.reshape(remeberedImage, (image.shape[0],image.shape[0]))
+	plt.subplot(241),plt.imshow(image,cmap = 'gray', interpolation = 'nearest')
+	plt.title('Orginal image'), plt.xticks([]), plt.yticks([])	
 
-	plt.subplot(121),plt.imshow(image,cmap = 'gray', interpolation = 'nearest')
-	plt.title('Thresholded Image'), plt.xticks([]), plt.yticks([])	
-
-	plt.subplot(122),plt.imshow(remeberedImage,cmap = 'gray', interpolation = 'nearest')
-	plt.title('Thresholded Image'), plt.xticks([]), plt.yticks([])
+	plt.subplot(245),plt.imshow(remeberedImage100,cmap = 'gray', interpolation = 'nearest')
+	plt.title('Image after 100 iterations'), plt.xticks([]), plt.yticks([])
+	plt.subplot(246),plt.imshow(remeberedImage300,cmap = 'gray', interpolation = 'nearest')
+	plt.title('Image after 300 iterations'), plt.xticks([]), plt.yticks([])
+	plt.subplot(247),plt.imshow(remeberedImage600,cmap = 'gray', interpolation = 'nearest')
+	plt.title('Image after 600 iterations'), plt.xticks([]), plt.yticks([])
+	plt.subplot(248),plt.imshow(remeberedImage1000,cmap = 'gray', interpolation = 'nearest')
+	plt.title('Image after 1000 iterations'), plt.xticks([]), plt.yticks([])
 	plt.show()
 
 if __name__ == '__main__':
